45e17a256acf2fd65c1ba2e8b07138a4622682ff
Davidpavan <David@zelarsoft.com>
2021-05-03 11:10:54 +0530

    #1 - Coloured syntax

--- 01-print.sh
+++ 01-print.sh
@@ -5,3 +5,8 @@ echo Hello world
 # single echo command but multiple line output
 
 echo -e "Hai\nBye"
+
+# Sometimes we need colours to be printed
+# syntax : "\e[colour number m message" and to disable the colour - 0m
+
+echo -e "\e[31mMaking fun of me\e[0m"
